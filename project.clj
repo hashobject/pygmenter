e913b06bd6f96355cd4c192b8f9ad789f111187b
Anton Podviaznikov <podviaznikov@gmail.com>
2013-06-28 01:02:41 -0600

    initial commit. no code yet

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject pygmenter "0.1.0-SNAPSHOT"
+  :description "A Clojure wrapper for Pygments (syntax highlighter)."
+  :signing {:gpg-key "HashObject Ltd <team@hashobject.com>"}
+  :url "https://github.com/hashobject/mangopay"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ test/pygmenter/core_test.clj
@@ -0,0 +1,7 @@
+(ns pygmenter.core-test
+  (:use clojure.test
+        pygmenter.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
