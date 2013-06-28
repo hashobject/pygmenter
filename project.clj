922a5022b139a763db574fed41c10068eb2b9afa
Anton Podviaznikov <podviaznikov@gmail.com>
2013-06-28 01:42:13 -0600

    fix url

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject pygmenter "0.1.0"
+(defproject pygmenter "0.1.1"
   :description "A Clojure wrapper for Pygments (syntax highlighter)."
   :signing {:gpg-key "HashObject Ltd <team@hashobject.com>"}
-  :url "https://github.com/hashobject/mangopay"
+  :url "https://github.com/hashobject/pygmenter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
