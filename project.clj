44fcebe08cf3d8ddf5395db569e5fb4a44abf26f
Anton Podviaznikov <podviaznikov@gmail.com>
2013-07-16 10:59:41 -0600

    update metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pygmenter "0.1.1"
   :description "A Clojure wrapper for Pygments (syntax highlighter)."
-  :signing {:gpg-key "HashObject Ltd <team@hashobject.com>"}
+  :signing {:gpg-key "Hashobject Ltd <team@hashobject.com>"}
   :url "https://github.com/hashobject/pygmenter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
